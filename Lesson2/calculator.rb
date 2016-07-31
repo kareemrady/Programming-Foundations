25da6e8aa85ede64a087005a03d8d7df0d8af006
Kareem Rady <kareem_rady@yahoo.com>
2016-07-30 18:00:40 -0600

    Corrected as per rubocop instructions

--- Lesson2/calculator.rb
+++ Lesson2/calculator.rb
@@ -1,8 +1,9 @@
-Kernel.puts("Please Type in 2 numbers seperated by a space ex: 2 3")
-numbers_string = Kernel.gets().chomp()
-numbers = numbers_string.split(" ").map{|num| num.to_i}
-Kernel.puts("Please type the required operation ex: + - / * ")
-operation = Kernel.gets().chomp()
+Kernel.puts 'Please Type in 2 numbers seperated by a space ex: 2 3'
+numbers_string = Kernel.gets.chomp
+numbers = numbers_string.split(' ').map(&:to_i)
+Kernel.puts 'Please type the required operation ex: + - / * '
+operation = Kernel.gets.chomp
 operation_fn = operation.to_sym
 result = numbers[0].send(operation_fn, numbers[1])
+puts
 puts result
